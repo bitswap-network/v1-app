f765806de5fb90287b83a4736b096a5a6c06ee3a
Vansh Sethi <44345210+Vansh-Sethi@users.noreply.github.com>
2021-04-20 00:40:22 -0400

    modal for transactions

--- src/pages/Wallet/index.tsx
+++ src/pages/Wallet/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../../App.css";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
 import TextField from "@material-ui/core/TextField";
 import axios from "axios";
 import env from "../../components/data/env.json";
@@ -8,12 +8,13 @@ import { Redirect } from "react-router-dom";
 import NavBar from "components/NavBar";
 import { loggedInState, userState } from "store";
 import { useRecoilValue } from "recoil";
-import { FiCodesandbox, FiActivity} from "react-icons/fi";
+import { FiCodesandbox, FiActivity, FiX} from "react-icons/fi";
 
 
 const Wallet = (props: any) => {
   const user = useRecoilValue(userState);
   const isLoggedIn = useRecoilValue(loggedInState);
+  const [modalOpen, setOpenModal] = useState(false);
 
  
   useEffect(() => {
@@ -22,6 +23,32 @@ const Wallet = (props: any) => {
 
 
   return (
+    <>
+      <Modal show={modalOpen} onHide={true}>
+        <Modal.Body>
+          <FiX size={'1rem'} onClick={()=>{setOpenModal(false)}} style={{float:'right', marginRight: "0.75rem", marginTop: "0.5rem", color: "#ACB5BD"}} />
+          <h4 style={{marginTop: "1.25rem", marginLeft: "1.2rem"}}><b>Transaction Details</b></h4>
+          <Col style={{display:'flex',flexDirection: "row", color: "#ACB5BD", fontSize: "0.7rem", marginTop: "1.5rem", marginLeft: "0.5rem"}}>
+            <Col sm={4}>
+              <Row>BUYER</Row>
+              <Row><p style={{fontSize: "0.75rem", color: "#495057"}}>@john.smith</p></Row>
+            </Col>
+            <Col sm={5}>
+              <Row>OFFER</Row>
+              <Row><p style={{fontSize: "0.75rem", color: "#495057"}}>40 @ $135.00 USD</p></Row>
+            </Col>
+            <Col sm={0}>
+              <Row>TIME</Row>
+              <Row><p style={{fontSize: "0.75rem", color: "#495057"}}>1 minute ago</p></Row>
+            </Col>
+          </Col>
+         
+
+         
+          
+        </Modal.Body>
+      </Modal>
+
     <Container
       style={
         window.innerWidth <= 768
@@ -34,6 +61,8 @@ const Wallet = (props: any) => {
             }
       }
     >
+     
+      
       <NavBar />
         <>
           <Col
@@ -136,7 +165,7 @@ const Wallet = (props: any) => {
               </Row>
               <Row style={{marginTop: "5%", marginLeft: "4%"}}>
                 <Col ><p style={{color: "#495057"}}>40</p></Col>
-                <Col><p style={{color: "#4263EB", fontSize: '1rem'}}>Details →</p></Col>
+                <Col><p style={{color: "#4263EB", fontSize: '1rem'}}><a onClick={()=>{setOpenModal(true)}}>Details →</a></p></Col>
               </Row>
               <Row>
                 <hr style={{borderTop: "1px solid #DDE2E5", width: "100rem"}} />
@@ -189,7 +218,7 @@ const Wallet = (props: any) => {
 
               <Row style={{marginTop: "5%", marginLeft: "4%"}}>
                 <Col ><p style={{color: "#495057"}}>420</p></Col>
-                <Col><p style={{color: "#4263EB", fontSize: '1rem'}}>Details →</p></Col>
+                <Col><p style={{color: "#4263EB", fontSize: '1rem'}}><a>Details →</a></p></Col>
               </Row>
               <Row>
                 <hr style={{borderTop: "1px solid #DDE2E5", width: "100rem"}} />
@@ -211,6 +240,7 @@ const Wallet = (props: any) => {
           </Col>
         </>
     </Container>
+    </>
   );
 };
 export default Wallet;

54e7067b7a34e169823f754f0639b969c838c0cd
Aryan Misra <aryanmisra4@gmail.com>
2021-04-20 17:35:45 -0400

    ok

--- src/components/modalTransactionInfo/index.tsx
+++ src/components/modalTransactionInfo/index.tsx
@@ -22,10 +22,22 @@ const TransactionModal: React.FC<TxnModal> = (
   { transaction, open, close },
   props: any
 ) => {
-  // const [openModal, setOpenModal] = useState(open);
-  // console.log(open, openModal);
+  const dateRender = (date: Date) => {
+    let diffTime =
+      Math.abs(new Date().getTime() - new Date(date).getTime()) / 1000;
+    if (diffTime < 60) {
+      return "<1 minute ago";
+    } else if (diffTime > 60 && diffTime < 3600) {
+      return `${diffTime / 60} minutes ago`;
+    } else if (diffTime > 3600 && diffTime < 86400) {
+      return `${(diffTime / 3600).toFixed(0)} hours ago`;
+    } else if (diffTime > 86400) {
+      return `${(diffTime / 86400).toFixed(0)} days ago`;
+    }
+  };
   const user = useRecoilValue(userState);
   const isLoggedIn = useRecoilValue(loggedInState);
+  console.log(transaction);
   return (
     <Modal show={open} onHide={close}>
       <Modal.Body>
@@ -38,45 +50,93 @@ const TransactionModal: React.FC<TxnModal> = (
             marginTop: "0.5rem",
             color: "#ACB5BD",
           }}
+          className="hoverCursor"
         />
-        <h4 style={{ marginTop: "1.25rem", marginLeft: "1.2rem" }}>
-          <b>Transaction Details</b>
-        </h4>
-        <Col
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            color: "#ACB5BD",
-            fontSize: "0.7rem",
-            marginTop: "1.5rem",
-            marginLeft: "0.5rem",
-          }}
-        >
-          <Col sm={4}>
-            <Row>BUYER</Row>
-            <Row>
-              <p style={{ fontSize: "0.75rem", color: "#495057" }}>
-                @john.smith
+        {transaction && (
+          <>
+            <h4 style={{ marginTop: "1.25rem", marginLeft: "1.2rem" }}>
+              <b>Transaction Details</b>
+              <p
+                style={{
+                  fontSize: "0.75rem",
+                  color: "#495057",
+                  marginTop: "1rem",
+                }}
+              >
+                ID: {transaction.tx_id}
               </p>
-            </Row>
-          </Col>
-          <Col sm={5}>
-            <Row>OFFER</Row>
-            <Row>
-              <p style={{ fontSize: "0.75rem", color: "#495057" }}>
-                40 @ $135.00 USD
-              </p>
-            </Row>
-          </Col>
-          <Col sm={0}>
-            <Row>TIME</Row>
-            <Row>
-              <p style={{ fontSize: "0.75rem", color: "#495057" }}>
-                1 minute ago
-              </p>
-            </Row>
-          </Col>
-        </Col>
+            </h4>
+
+            <Col
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                color: "#ACB5BD",
+                fontSize: "0.7rem",
+                marginTop: "1.5rem",
+                marginLeft: "0.5rem",
+              }}
+            >
+              <Col sm={4}>
+                <Row>TXN TYPE</Row>
+                <Row>
+                  <p style={{ fontSize: "0.75rem", color: "#495057" }}>
+                    {transaction.transactiontype}
+                  </p>
+                </Row>
+              </Col>
+              <Col sm={5}>
+                <Row>AMOUNT</Row>
+                <Row>
+                  <p style={{ fontSize: "0.75rem", color: "#495057" }}>
+                    {transaction.bitcloutnanos / 1e9} $BTCLT
+                  </p>
+                </Row>
+              </Col>
+              <Col sm={4}>
+                <Row>STATUS</Row>
+                <Row>
+                  <p style={{ fontSize: "0.75rem", color: "#495057" }}>
+                    {transaction.status}
+                  </p>
+                </Row>
+              </Col>
+            </Col>
+            <Col
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                color: "#ACB5BD",
+                fontSize: "0.7rem",
+                marginTop: "1.5rem",
+                marginLeft: "0.5rem",
+              }}
+            >
+              <Col sm={0}>
+                <Row>START TIME</Row>
+                <Row>
+                  <p style={{ fontSize: "0.75rem", color: "#495057" }}>
+                    {new Date(transaction.created).toLocaleDateString()}:{" "}
+                    {new Date(transaction.created).toLocaleTimeString()}
+                  </p>
+                </Row>
+              </Col>
+              <Col>
+                {transaction.status === "completed" && (
+                  <>
+                    <Row> COMPLETED TIME</Row>
+                    <Row>
+                      <p style={{ fontSize: "0.75rem", color: "#495057" }}>
+                        {new Date(transaction.completed).toLocaleDateString()}:{" "}
+                        {new Date(transaction.completed).toLocaleTimeString()}
+                      </p>
+                    </Row>
+                  </>
+                )}
+              </Col>
+            </Col>
+          </>
+        )}
       </Modal.Body>
     </Modal>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,13 +8,19 @@ import {
   Button,
   InputGroup,
   FormControl,
-  Modal
+  Modal,
 } from "react-bootstrap";
 import TextField from "@material-ui/core/TextField";
 import FeedListing from "../../components/FeedListing";
 import { ListingSchema } from "../../components/interfaces";
 import env from "../../components/data/env.json";
-import { FiBookmark, FiX, FiChevronUp, FiChevronDown, FiDollarSign } from "react-icons/fi";
+import {
+  FiBookmark,
+  FiX,
+  FiChevronUp,
+  FiChevronDown,
+  FiDollarSign,
+} from "react-icons/fi";
 import NavBar from "../../components/NavBar";
 import MediaQuery from "react-responsive";
 import { getListings, createListing } from "../../services/listings";
@@ -34,8 +40,7 @@ const Home = (props: any) => {
   const isLoggedIn = useRecoilValue(loggedInState);
   const [listings, setListings] = useState<ListingSchema[]>([]);
   const [loading, setLoading] = useState(true);
-  const [bitcloutprice, setBitcloutprice] = useState(0);
-  const [etherPrice, setetherPrice] = useState(0);
+
   const [volumeSort, setVolumeSort] = useState("desc");
   const [dateSort, setDateSort] = useState("desc");
   useEffect(() => {
@@ -80,181 +85,338 @@ const Home = (props: any) => {
 
   return (
     <>
-    <Modal show={false} onHide={true}>
+      <Modal show={false} onHide={true}>
         <Modal.Body>
-          <FiX size={'1rem'} style={{float:'right', marginRight: "0.75rem", marginTop: "0.5rem", color: "#ACB5BD"}} />
-          <h3 style={{marginTop: "1.7rem", marginLeft: "2rem"}}><b>Confirm Payment</b></h3>
-          <p style={{color: "#212429", fontSize: '0.7rem', marginLeft: "2rem", marginTop: "0.75rem"}}>By clicking confirm, you will agree to purchase the following BitClout.</p>
-          <Col style={{display:'flex',flexDirection: "row", color: "#ACB5BD", fontSize: "0.7rem", marginTop: "1.5rem", marginLeft: "0.5rem"}}>
+          <FiX
+            size={"1rem"}
+            style={{
+              float: "right",
+              marginRight: "0.75rem",
+              marginTop: "0.5rem",
+              color: "#ACB5BD",
+            }}
+          />
+          <h3 style={{ marginTop: "1.7rem", marginLeft: "2rem" }}>
+            <b>Confirm Payment</b>
+          </h3>
+          <p
+            style={{
+              color: "#212429",
+              fontSize: "0.7rem",
+              marginLeft: "2rem",
+              marginTop: "0.75rem",
+            }}
+          >
+            By clicking confirm, you will agree to purchase the following
+            BitClout.
+          </p>
+          <Col
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              color: "#ACB5BD",
+              fontSize: "0.7rem",
+              marginTop: "1.5rem",
+              marginLeft: "0.5rem",
+            }}
+          >
             <Col sm={4}>
-              <Row style={{justifyContent: "center"}}>
-              <FiDollarSign size={'1.5rem'} style={{color: "#212429", marginTop: "0.4rem"}} />
-              <p style={{color: "#212429", fontSize: "1.5rem"}}>135</p>
-              <p style={{color: "#212429", fontSize: "0.9rem", marginLeft: "0.4rem", marginTop: "0.7rem"}}>USD</p>
+              <Row style={{ justifyContent: "center" }}>
+                <FiDollarSign
+                  size={"1.5rem"}
+                  style={{ color: "#212429", marginTop: "0.4rem" }}
+                />
+                <p style={{ color: "#212429", fontSize: "1.5rem" }}>135</p>
+                <p
+                  style={{
+                    color: "#212429",
+                    fontSize: "0.9rem",
+                    marginLeft: "0.4rem",
+                    marginTop: "0.7rem",
+                  }}
+                >
+                  USD
+                </p>
               </Row>
 
-              <Row><p style={{fontSize: "0.75rem", color: "#495057"}}>@john.smith</p></Row>
+              <Row>
+                <p style={{ fontSize: "0.75rem", color: "#495057" }}>
+                  @john.smith
+                </p>
+              </Row>
             </Col>
             <Col sm={5}>
               <Row>OFFER</Row>
-              <Row><p style={{fontSize: "0.75rem", color: "#495057"}}>40 @ $135.00 USD</p></Row>
+              <Row>
+                <p style={{ fontSize: "0.75rem", color: "#495057" }}>
+                  40 @ $135.00 USD
+                </p>
+              </Row>
             </Col>
-           
           </Col>
-         
-
-         
-          
         </Modal.Body>
       </Modal>
-    <Wrapper>
-      <NavBar />
-      <Col>
-        <MainContent sm={12}>
-          <Row>
-            <h3
-              style={
-                window.visualViewport.width <= 768
-                  ? { marginLeft: "2.5rem" }
-                  : {}
-              }
-            >
-              <b>Swap Feed</b>
-            </h3>
-            <h5
-              style={
-                window.visualViewport.width <= 768
-                  ? { marginLeft: "2.5rem" }
-                  : {}
-              }
-            >
-              {/* $Bitclout price: ~${bitcloutprice.toFixed(2)} */}
-            </h5>
-            <MediaQuery query="(min-device-width: 768px)">
-              <DesktopButton
+      <Wrapper>
+        <NavBar />
+        <Col>
+          <MainContent sm={12}>
+            <Row>
+              <h3
+                style={
+                  window.visualViewport.width <= 768
+                    ? { marginLeft: "2.5rem" }
+                    : {}
+                }
+              >
+                <b>Swap Feed</b>
+              </h3>
+              <h5
+                style={
+                  window.visualViewport.width <= 768
+                    ? { marginLeft: "2.5rem" }
+                    : {}
+                }
+              >
+                {/* $Bitclout price: ~${bitcloutprice.toFixed(2)} */}
+              </h5>
+              <MediaQuery query="(min-device-width: 768px)">
+                <DesktopButton
+                  onClick={() => {
+                    // setPostAdPart(true);
+                  }}
+                >
+                  Post Swap
+                </DesktopButton>
+              </MediaQuery>
+            </Row>
+            <MediaQuery query="(max-device-width: 768px)">
+              <MobileButton
                 onClick={() => {
-                  // setPostAdPart(true);
+                  props.history.push("/postad");
                 }}
               >
                 Post Swap
-              </DesktopButton>
+              </MobileButton>
             </MediaQuery>
+            <FeedContent>
+              <Col>
+                <SearchBarWrapper>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                      <InputGroup.Text id="basic-addon1">
+                        <FiBookmark size={20} color={"#43494f"} />
+                      </InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                      placeholder="Search Offers"
+                      aria-label="Search Offers"
+                      aria-describedby="basic-addon1"
+                    />
+                  </InputGroup>
+                </SearchBarWrapper>
+
+                <div
+                  className="scrollNoBar"
+                  style={{ background: "transparent" }}
+                >
+                  <Row style={{ marginBottom: "-1.2em", marginLeft: "1em" }}>
+                    <p
+                      style={
+                        window.visualViewport.width <= 768
+                          ? {
+                              marginRight: "6rem",
+                              color: "#C4C4C4",
+                              fontSize: "0.8em",
+                            }
+                          : {
+                              color: "#C4C4C4",
+                              marginRight: "22em",
+                              fontSize: "0.8em",
+                            }
+                      }
+                    >
+                      Username
+                    </p>
+
+                    <p
+                      style={
+                        window.visualViewport.width <= 768
+                          ? { color: "#C4C4C4", fontSize: "0.8em" }
+                          : {
+                              color: "#C4C4C4",
+                              marginRight: "23em",
+                              fontSize: "0.8em",
+                            }
+                      }
+                    >
+                      Offer
+                    </p>
+
+                    <p
+                      style={
+                        window.visualViewport.width <= 768
+                          ? { display: "none" }
+                          : { color: "#C4C4C4", fontSize: "0.8em" }
+                      }
+                    >
+                      Posted Time
+                      {dateSort === "desc" ? (
+                        <FiChevronDown
+                          size={20}
+                          color={"black"}
+                          onClick={() => handleSort("date")}
+                        />
+                      ) : (
+                        <FiChevronUp
+                          size={20}
+                          color={"black"}
+                          onClick={() => handleSort("date")}
+                        />
+                      )}
+                    </p>
+                  </Row>
+                  {listings.map((listing: any, i: number) => (
+                    <FeedListing
+                      listing={listing}
+                      price={1}
+                      index={i}
+                      key={i}
+                      loading={loading}
+                      history={props.history}
+                    />
+                  ))}
+                </div>
+              </Col>
+            </FeedContent>
+          </MainContent>
+        </Col>
+        <Col style={{ marginLeft: "-10%" }}>
+          <Row>
+            <div
+              style={{
+                borderRight: "1px solid #DDE2E5",
+                height: "100vh",
+                paddingRight: 0,
+                width: "2rem",
+              }}
+            />
           </Row>
-          <MediaQuery query="(max-device-width: 768px)">
-            <MobileButton
-              onClick={() => {
-                props.history.push("/postad");
+        </Col>
+        <Col sm={4} style={{ marginTop: "6%" }}>
+          <Row>
+            <h5 style={{ fontWeight: 600, marginLeft: "10%" }}>
+              Ongoing Swaps
+            </h5>
+          </Row>
+          <Row>
+            <p
+              style={{
+                color: "#ACB5BD",
+                fontSize: "0.75rem",
+                marginTop: "12%",
+                marginLeft: "10%",
               }}
             >
-              Post Swap
-            </MobileButton>
-          </MediaQuery>
-          <FeedContent>
-            <Col>
-              <SearchBarWrapper>
-                <InputGroup className="mb-3">
-                  <InputGroup.Prepend>
-                    <InputGroup.Text id="basic-addon1">
-                      <FiBookmark size={20} color={"#43494f"} />
-                    </InputGroup.Text>
-                  </InputGroup.Prepend>
-                  <FormControl
-                    placeholder="Search Offers"
-                    aria-label="Search Offers"
-                    aria-describedby="basic-addon1"
-                  />
-                </InputGroup>
-              </SearchBarWrapper>
+              Amount (BCL)
+            </p>
+          </Row>
+          <div className="scrollNoBarSplit">
+            <Row>
+              <hr style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }} />
+            </Row>
+            <Row style={{ marginTop: "5%", marginLeft: "4%" }}>
+              <Col>
+                <p style={{ color: "#495057" }}>5 BCL & $10</p>
+              </Col>
+              <Col>
+                <p style={{ color: "#4263EB", fontSize: "1rem" }}>View →</p>
+              </Col>
+            </Row>
+            <Row>
+              <hr style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }} />
+            </Row>
 
-              <div
-                className="scrollNoBar"
-                style={{ background: "transparent" }}
-              >
-                <Row style={{ marginBottom: "-1.2em", marginLeft: "1em" }}>
-                  <p
-                    style={
-                      window.visualViewport.width <= 768
-                        ? {
-                            marginRight: "6rem",
-                            color: "#C4C4C4",
-                            fontSize: "0.8em",
-                          }
-                        : {
-                            color: "#C4C4C4",
-                            marginRight: "22em",
-                            fontSize: "0.8em",
-                          }
-                    }
-                  >
-                    Username
-                  </p>
+            <Row style={{ marginTop: "5%", marginLeft: "4%" }}>
+              <Col>
+                <p style={{ color: "#495057" }}>5 BCL & $10</p>
+              </Col>
+              <Col>
+                <p style={{ color: "#4263EB", fontSize: "1rem" }}>View →</p>
+              </Col>
+            </Row>
+            <Row>
+              <hr style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }} />
+            </Row>
+            <Row style={{ marginTop: "5%", marginLeft: "4%" }}>
+              <Col>
+                <p style={{ color: "#495057" }}>5 BCL & $10</p>
+              </Col>
+              <Col>
+                <p style={{ color: "#4263EB", fontSize: "1rem" }}>View →</p>
+              </Col>
+            </Row>
+            <Row>
+              <hr style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }} />
+            </Row>
 
-                  <p
-                    style={
-                      window.visualViewport.width <= 768
-                        ? { color: "#C4C4C4", fontSize: "0.8em" }
-                        : {
-                            color: "#C4C4C4",
-                            marginRight: "23em",
-                            fontSize: "0.8em",
-                          }
-                    }
-                  >
-                    Offer
-                    {/* {volumeSort === "desc" ? (
-                      <FiChevronDown
-                        size={20}
-                        color={"black"}
-                        onClick={() => handleSort("volume")}
-                      />
-                    ) : (
-                      <FiChevronUp
-                        size={20}
-                        color={"black"}
-                        onClick={() => handleSort("volume")}
-                      />
-                    )} */}
-                  </p>
+            <Row style={{ marginTop: "5%", marginLeft: "4%" }}>
+              <Col>
+                <p style={{ color: "#495057" }}>5 BCL & $10</p>
+              </Col>
+              <Col>
+                <p style={{ color: "#4263EB", fontSize: "1rem" }}>View →</p>
+              </Col>
+            </Row>
+            <Row>
+              <hr style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }} />
+            </Row>
 
-                  <p
-                    style={
-                      window.visualViewport.width <= 768
-                        ? { display: "none" }
-                        : { color: "#C4C4C4", fontSize: "0.8em" }
-                    }
-                  >
-                    Posted Time
-                    {dateSort === "desc" ? (
-                      <FiChevronDown
-                        size={20}
-                        color={"black"}
-                        onClick={() => handleSort("date")}
-                      />
-                    ) : (
-                      <FiChevronUp
-                        size={20}
-                        color={"black"}
-                        onClick={() => handleSort("date")}
-                      />
-                    )}
-                  </p>
-                </Row>
-                {listings.map((listing: any, i: number) => (
-                  <FeedListing
-                    listing={listing}
-                    price={1}
-                    index={i}
-                    key={i}
-                    loading={loading}
-                    history={props.history}
-                  />
-                ))}
-              </div>
-            </Col>
-          </FeedContent>
-        </MainContent>
-      </Col>
-    </Wrapper>
+            <Row style={{ marginTop: "5%", marginLeft: "4%" }}>
+              <Col>
+                <p style={{ color: "#495057" }}>5 BCL & $10</p>
+              </Col>
+              <Col>
+                <p style={{ color: "#4263EB", fontSize: "1rem" }}>View →</p>
+              </Col>
+            </Row>
+            <Row>
+              <hr style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }} />
+            </Row>
+
+            <Row style={{ marginTop: "5%", marginLeft: "4%" }}>
+              <Col>
+                <p style={{ color: "#495057" }}>5 BCL & $10</p>
+              </Col>
+              <Col>
+                <p style={{ color: "#4263EB", fontSize: "1rem" }}>View →</p>
+              </Col>
+            </Row>
+            <Row>
+              <hr style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }} />
+            </Row>
+          </div>
+
+          <Row style={{ marginTop: "7.5%" }}>
+            <h5 style={{ fontWeight: 600, marginLeft: "10%" }}>Ongoing Buy</h5>
+          </Row>
+          <div className="scrollNoBarSplit">
+            <Row>
+              <hr style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }} />
+            </Row>
+            <Row style={{ marginTop: "5%", marginLeft: "4%" }}>
+              <Col>
+                <p style={{ color: "#495057" }}>5 @ $10</p>
+              </Col>
+              <Col>
+                <p style={{ color: "#4263EB", fontSize: "1rem" }}>View →</p>
+              </Col>
+            </Row>
+            <Row>
+              <hr style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }} />
+            </Row>
+          </div>
+        </Col>
+      </Wrapper>
     </>
   );
 };

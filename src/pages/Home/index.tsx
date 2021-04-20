1e8070909d6a27685602f3895c489dd9335c589f
Vansh Sethi <44345210+Vansh-Sethi@users.noreply.github.com>
2021-04-20 01:06:52 -0400

    modal for buy

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,12 +8,13 @@ import {
   Button,
   InputGroup,
   FormControl,
+  Modal
 } from "react-bootstrap";
 import TextField from "@material-ui/core/TextField";
 import FeedListing from "../../components/FeedListing";
 import { ListingSchema } from "../../components/interfaces";
 import env from "../../components/data/env.json";
-import { FiBookmark, FiX, FiChevronUp, FiChevronDown } from "react-icons/fi";
+import { FiBookmark, FiX, FiChevronUp, FiChevronDown, FiDollarSign } from "react-icons/fi";
 import NavBar from "../../components/NavBar";
 import MediaQuery from "react-responsive";
 import { getListings, createListing } from "../../services/listings";
@@ -79,6 +80,34 @@ const Home = (props: any) => {
   }, [dateSort, volumeSort]);
 
   return (
+    <>
+    <Modal show={false} onHide={true} size={'md'}>
+        <Modal.Body>
+          <FiX size={'1rem'} style={{float:'right', marginRight: "0.75rem", marginTop: "0.5rem", color: "#ACB5BD"}} />
+          <h3 style={{marginTop: "1.7rem", marginLeft: "2rem"}}><b>Confirm Payment</b></h3>
+          <p style={{color: "#212429", fontSize: '0.7rem', marginLeft: "2rem", marginTop: "0.75rem"}}>By clicking confirm, you will agree to purchase the following BitClout.</p>
+          <Col style={{display:'flex',flexDirection: "row", color: "#ACB5BD", fontSize: "0.7rem", marginTop: "1.5rem", marginLeft: "0.5rem"}}>
+            <Col sm={4}>
+              <Row style={{justifyContent: "center"}}>
+              <FiDollarSign size={'1.5rem'} style={{color: "#212429", marginTop: "0.4rem"}} />
+              <p style={{color: "#212429", fontSize: "1.5rem"}}>135</p>
+              <p style={{color: "#212429", fontSize: "0.9rem", marginLeft: "0.4rem", marginTop: "0.7rem"}}>USD</p>
+              </Row>
+
+              <Row><p style={{fontSize: "0.75rem", color: "#495057"}}>@john.smith</p></Row>
+            </Col>
+            <Col sm={5}>
+              <Row>OFFER</Row>
+              <Row><p style={{fontSize: "0.75rem", color: "#495057"}}>40 @ $135.00 USD</p></Row>
+            </Col>
+           
+          </Col>
+         
+
+         
+          
+        </Modal.Body>
+      </Modal>
     <Wrapper>
       <NavBar />
       <Col>
@@ -227,6 +256,7 @@ const Home = (props: any) => {
         </MainContent>
       </Col>
     </Wrapper>
+    </>
   );
 };
 

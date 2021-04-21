001c7b6c777c0dc12d1d92b9cdd06f208736c801
Aryan Misra <aryanmisra4@gmail.com>
2021-04-21 16:10:43 -0400

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,7 @@ import {
   Button,
   InputGroup,
   FormControl,
-  Modal
+  Modal,
 } from "react-bootstrap";
 import FeedListing from "../../components/FeedListing";
 import { ListingSchema } from "../../components/interfaces";
@@ -16,7 +16,7 @@ import {
   FiChevronUp,
   FiChevronDown,
   FiDollarSign,
-  FiBox
+  FiBox,
 } from "react-icons/fi";
 import NavBar from "../../components/NavBar";
 import { Redirect } from "react-router-dom";
@@ -30,7 +30,7 @@ import {
   DesktopButton,
   MobileButton,
   FeedContent,
-  SearchBarWrapper
+  SearchBarWrapper,
 } from "./styles";
 import { useUser } from "components/hooks";
 import OngoingItem from "components/OngoingItem";
@@ -47,12 +47,12 @@ const Home = (props: any) => {
 
   useEffect(() => {
     getListings(volumeSort, dateSort)
-      .then(res => {
+      .then((res) => {
         console.log(res);
         setListings(res.data);
         setLoading(false);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         setLoading(false);
       });
@@ -138,7 +138,7 @@ const Home = (props: any) => {
                           ? {
                               marginRight: "6rem",
                               color: "#C4C4C4",
-                              fontSize: "0.8em"
+                              fontSize: "0.8em",
                             }
                           : {
                               color: "#C4C4C4",
@@ -146,7 +146,7 @@ const Home = (props: any) => {
                                 window.visualViewport.width <= 1800
                                   ? "10em"
                                   : "22em",
-                              fontSize: "0.8em"
+                              fontSize: "0.8em",
                             }
                       }
                     >
@@ -163,7 +163,7 @@ const Home = (props: any) => {
                                 window.visualViewport.width <= 1800
                                   ? "14em"
                                   : "22em",
-                              fontSize: "0.8em"
+                              fontSize: "0.8em",
                             }
                       }
                     >
@@ -222,13 +222,13 @@ const Home = (props: any) => {
                       height: "100vh",
                       paddingRight: 0,
                       width: "2rem",
-                      marginLeft: "0.25rem"
+                      marginLeft: "0.25rem",
                     }
                   : {
                       borderRight: "1px solid #DDE2E5",
                       height: "100vh",
                       paddingRight: 0,
-                      width: "2rem"
+                      width: "2rem",
                     }
               }
             />
@@ -247,7 +247,7 @@ const Home = (props: any) => {
                   color: "#ACB5BD",
                   fontSize: "0.75rem",
                   marginTop: "12%",
-                  marginLeft: "10%"
+                  marginLeft: "10%",
                 }}
               >
                 Amount (BTCLT)
@@ -259,7 +259,7 @@ const Home = (props: any) => {
                   style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }}
                 />
               </Row>
-              {userData?.listings.map(listing => {
+              {userData?.listings.map((listing) => {
                 if (listing.ongoing) {
                   return (
                     <OngoingItem
@@ -270,7 +270,9 @@ const Home = (props: any) => {
                   );
                 }
               })}
-              {!userData?.listings.some(listing => listing.ongoing === true) ? (
+              {!userData?.listings.some(
+                (listing) => listing.ongoing === true
+              ) ? (
                 <p>You don't have any ongoing swaps</p>
               ) : null}
               {/* <Row style={{ marginTop: "5%", marginLeft: "4%" }}>
@@ -354,12 +356,10 @@ const Home = (props: any) => {
                   style={{ borderTop: "1px solid #DDE2E5", width: "100rem" }}
                 />
               </Row>
-              {listings.map(listing => {
-                if (
-                  listing.buyer._id === userData._id &&
-                  listing.ongoing === true
-                ) {
-                  return (
+              {listings.map((listing) => {
+                // {listing.buyer&&
+                if (listing.ongoing === true) {
+                  listing.buyer._id === userData._id && (
                     <OngoingItem
                       bitcloutnanos={listing.bitcloutnanos}
                       usdamount={listing.usdamount}
@@ -367,9 +367,10 @@ const Home = (props: any) => {
                     />
                   );
                 }
+                // }
               })}
               {!listings.some(
-                listing =>
+                (listing) =>
                   listing.buyer._id === userData._id && listing.ongoing === true
               ) ? (
                 <p>You don't have any ongoing buys</p>

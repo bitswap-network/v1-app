184aa09fc0a9a8163d45d772a2332773b1ed85ae
Alex Yu <alexjy@yahoo.com>
2021-04-29 03:01:06 -0400

    o

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,7 @@ import {
   FormControl,
   Modal,
   OverlayTrigger,
-  Tooltip,
+  Tooltip
 } from "react-bootstrap";
 import FeedListing from "../../components/FeedListing";
 import { ListingSchema } from "../../components/interfaces";
@@ -19,7 +19,7 @@ import {
   FiChevronDown,
   FiDollarSign,
   FiBox,
-  FiHelpCircle,
+  FiHelpCircle
 } from "react-icons/fi";
 import NavBar from "../../components/NavBar";
 import { Redirect } from "react-router-dom";
@@ -32,12 +32,12 @@ import { useUser } from "components/hooks";
 import OngoingItem from "components/OngoingItem";
 import ModalError from "components/modalError/index";
 
-const ongoingSwapTooltip = (props) => (
+const ongoingSwapTooltip = props => (
   <Tooltip id="swap-tooltip" {...props}>
     These are listings you have made that are currently in progress
   </Tooltip>
 );
-const ongoingBuysTooltip = (props) => (
+const ongoingBuysTooltip = props => (
   <Tooltip id="buys-tooltip" {...props}>
     These are listings you have purchased that are currently in progress
   </Tooltip>
@@ -54,12 +54,12 @@ const Home = (props: any) => {
   console.log("user data", userData, user);
   useEffect(() => {
     getListings(volumeSort, dateSort)
-      .then((res) => {
+      .then(res => {
         console.log(res);
         setListings(res.data);
         setLoading(false);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
         setLoading(false);
       });
@@ -101,7 +101,11 @@ const Home = (props: any) => {
               <h3
                 style={
                   window.visualViewport.width <= 768
-                    ? { marginLeft: "1rem", fontSize: "1.5rem", marginBottom: "2rem" }
+                    ? {
+                        marginLeft: "1rem",
+                        fontSize: "1.5rem",
+                        marginBottom: "2rem"
+                      }
                     : { marginLeft: "1rem" }
                 }
               >
@@ -144,7 +148,7 @@ const Home = (props: any) => {
                   style={{
                     background: "transparent",
                     minHeight: "77vh",
-                    overflowX: "hidden",
+                    overflowX: "hidden"
                   }}
                 >
                   <Row style={{ marginBottom: "-1.2em", marginLeft: "1.3em" }}>
@@ -155,7 +159,7 @@ const Home = (props: any) => {
                             paddingBottom: "5%",
                             fontSize: "0.8em",
                             color: "#C4C4C4",
-                            width: "23%",
+                            width: "23%"
                           }}
                         >
                           Username
@@ -166,7 +170,7 @@ const Home = (props: any) => {
                             paddingBottom: "5%",
                             fontSize: "0.8em",
                             color: "#C4C4C4",
-                            width: "24%",
+                            width: "24%"
                           }}
                         >
                           Offer
@@ -176,7 +180,7 @@ const Home = (props: any) => {
                           style={{
                             paddingBottom: "5%",
                             fontSize: "0.8em",
-                            color: "#C4C4C4",
+                            color: "#C4C4C4"
                           }}
                         >
                           Posted Time
@@ -215,19 +219,24 @@ const Home = (props: any) => {
                       borderRight: "1px solid #DDE2E5",
                       height: "100vh",
                       paddingRight: 0,
-                      width: "2rem",
+                      width: "2rem"
                     }
-                    :
-
-                  {
-                    display: "none",
-                  }
+                  : {
+                      display: "none"
+                    }
               }
             />
           </Row>
         </Col>
         {isLoggedIn ? (
-          <Col sm={4} style={ window.visualViewport.width > 768 ? { marginTop: "6%" } : { marginTop: "-12%"}}>
+          <Col
+            sm={4}
+            style={
+              window.visualViewport.width > 768
+                ? { marginTop: "6%" }
+                : { marginTop: "-12%" }
+            }
+          >
             {userData && (
               <>
                 <Row>
@@ -235,12 +244,12 @@ const Home = (props: any) => {
                     Ongoing Swaps
                   </h5>
                   <OverlayTrigger
-                    placement="left"
+                    placement="bottom"
                     delay={{ show: 100, hide: 300 }}
                     overlay={ongoingSwapTooltip}
                   >
                     <FiHelpCircle
-                      style={{ marginTop: "0px", marginLeft: "12px" }}
+                      style={{ marginTop: "0.25em", marginLeft: "12px" }}
                     />
                   </OverlayTrigger>
                 </Row>
@@ -251,7 +260,7 @@ const Home = (props: any) => {
                       color: "#ACB5BD",
                       fontSize: "0.75rem",
                       marginTop: "12%",
-                      marginLeft: "10%",
+                      marginLeft: "10%"
                     }}
                   >
                     Amount (BCLT)
@@ -262,11 +271,11 @@ const Home = (props: any) => {
                     <hr
                       style={{
                         borderTop: "1px solid #DDE2E5",
-                        width: "100rem",
+                        width: "100rem"
                       }}
                     />
                   </Row>
-                  {userData.listings.map((listing) => {
+                  {userData.listings.map(listing => {
                     if (listing.ongoing) {
                       return (
                         <OngoingItem
@@ -278,7 +287,7 @@ const Home = (props: any) => {
                     }
                   })}
                   {userData.listings.some(
-                    (listing) => listing.ongoing === true
+                    listing => listing.ongoing === true
                   ) ? null : (
                     <p style={{ marginLeft: "5%", fontSize: "0.9rem" }}>
                       You don't have any ongoing swaps
@@ -291,12 +300,12 @@ const Home = (props: any) => {
                     Ongoing Buy
                   </h5>
                   <OverlayTrigger
-                    placement="left"
+                    placement="bottom"
                     delay={{ show: 100, hide: 300 }}
                     overlay={ongoingBuysTooltip}
                   >
                     <FiHelpCircle
-                      style={{ marginTop: "0px", marginLeft: "12px" }}
+                      style={{ marginTop: "0.25em", marginLeft: "12px" }}
                     />
                   </OverlayTrigger>
                 </Row>
@@ -306,7 +315,7 @@ const Home = (props: any) => {
                       color: "#ACB5BD",
                       fontSize: "0.75rem",
                       marginTop: "12%",
-                      marginLeft: "10%",
+                      marginLeft: "10%"
                     }}
                   >
                     Amount (BCLT)
@@ -317,11 +326,11 @@ const Home = (props: any) => {
                     <hr
                       style={{
                         borderTop: "1px solid #DDE2E5",
-                        width: "100rem",
+                        width: "100rem"
                       }}
                     />
                   </Row>
-                  {userData.buys.map((listing) =>
+                  {userData.buys.map(listing =>
                     listing.ongoing ? (
                       <OngoingItem
                         bitcloutnanos={listing.bitcloutnanos}
@@ -331,7 +340,7 @@ const Home = (props: any) => {
                     ) : null
                   )}
                   {userData.buys.some(
-                    (listing) => listing.ongoing === true
+                    listing => listing.ongoing === true
                   ) ? null : (
                     <p style={{ marginLeft: "5%", fontSize: "0.9rem" }}>
                       You don't have any ongoing buys

4c75cd0c542485d72c6836062a771170adb6ecfc
William Wang <william.wang@shopify.com>
2021-05-07 21:29:05 -0400

    Revert "Patch bitswap temporarily for maintenance mode"

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,19 @@
 import App from "./App";
 import ReactDOM from "react-dom";
 import React from "react";
-// import "./index.css";
+import "./index.css";
 import { RecoilRoot } from "recoil";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <RecoilRoot>
-//       <React.Suspense fallback={<></>}>
-//         <App />
-//       </React.Suspense>
-//     </RecoilRoot>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <React.Suspense fallback={<></>}>
+        <App />
+      </React.Suspense>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
